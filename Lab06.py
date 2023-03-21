52c764a8af30016096aa2275560e0ef0bc92ff36
Gatorxiwen <wuxiwen@ufl.edu>
2023-03-21 12:03:46 -0400

    intial comment

---
+++ Lab06.py
@@ -0,0 +1,37 @@
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+def decode(password):
+    decoded_password = ""
+    for digit in password:
+        decoded_digit = str(int(digit) - 3)
+        decoded_password += decoded_digit
+    return decoded_password
+
+def main():
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        x = int(input("Please enter an option: "))
+        if (x == 1):
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+            print()
+        if (x == 2):
+            print(f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.")
+            print()
+        if (x == 3):
+            break
+
+if __name__ == "__main__":
+    main()
+
